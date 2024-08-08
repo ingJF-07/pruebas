116bf1d45fe35f95f98e38f7965c54c91b64df17
francisco <ingjose.jv@gmail.com>
2024-08-07 22:41:06 -0600

    actualizacion v2

--- docs/assets/index-Aykt0jh9.js
+++ docs/assets/index-Aykt0jh9.js
@@ -6,12 +6,12 @@
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${n}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hola mundo</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      haz clic aqui.
     </p>
   </div>
 `;a(document.querySelector("#counter"));
